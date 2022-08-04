db98fcbf42ee3253be66ef57be25a7c822297091
lucas-leoni <lucas.leoni09@gmail.com>
2022-08-03 22:00:37 -0300

    socket com erro

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const app = require('./config/server');
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log('app start => http://localhost:5000');
+});
+
+// START SOCKET
+
+const io = require('socket.io')(server)
+
+// setando a variável socket io para que toda a minha app possa usar também
+app.set('io', io);
+
+io.on('connection', (socket) => {
+  console.log('entrei na sala');
+
+  socket.on('disconnect', () => {
+    console.log('saiu da sala');
+  });
+
+  socket.on('msgParaServidor', (data) => {
+    socket.emit('msgParaCliente', {
+      apelido: data.apelido,
+      mensagem: data.mensagem,
+    });
+  });
+
+  // disparando uma mensagem para todos os usuários
+  socket.broadcast.emit('msgParaCliente', {
+    apelido: data.apelido,
+    mensagem: data.mensagem,
+  });
 });
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,21 @@
 module.exports.iniciaChat = (application, req, res) => {
-  res.render('chat');
+  const dadosForm = req.body;
+  console.log(dadosForm);
+
+  req.assert('apelido', 'O campo apelido ou nome devem ser preenchido').notEmpty();
+  req.assert('apelido', 'O campo apelido ou nome devem conter de 3 a 15 caracteres').len(3,14);
+
+  const erros = req.validationErrors();
+
+  if(erros) {
+    res.render('index', { validacao: erros });
+    return;
+  }
+
+  application.get('io').emit('msgParaCliente', {
+    apelido: dadosForm.apelido,
+    mensagem: 'Entrou no chat',
+  });
+  
+  res.render('chat', { dadosForm });
 }
